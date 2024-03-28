8f0f1375c5bb303c0f93509ca6aaa45e5d8342a6
NyimLhAmo <03230178.gcbs@rub.edu.bt>
2024-03-28 20:20:00 +0600

    strings expamle

---
+++ stringsEx.py
@@ -0,0 +1,8 @@
+# get user input
+# convert the user input into all upper case 
+# print the convert string 
+# from cgi import test
+
+userInput = input("Enter a string")
+userInputConverted = userInput.upper()
+print(userInputConverted)
